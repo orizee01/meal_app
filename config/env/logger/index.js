2ad612dcc05707a153fb228a38ef874f3b1673f0
ori2ee01 <oreoluwa>
2023-11-14 14:52:55 +0100

    fixes

--- config/env/logger/index.js
+++ config/env/logger/index.js
@@ -3,7 +3,7 @@ const  { prodLogger, devLogger, testLogger } = require("./logger");
 const { NODE_ENV } = process.env;
 
 const logger = devLogger()
- NODE_ENV === "development" ? devLogger() : NODE_ENV === "production" ? prodLogger() : testLogger();
+//  NODE_ENV === "development" ? devLogger() : NODE_ENV === "production" ? prodLogger() : testLogger();
 
 module.exports = logger     
 
